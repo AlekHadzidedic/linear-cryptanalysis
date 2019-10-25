237a52975a7c2d481c4e3835522f1fcff66e9be1
Alek Hadzidedic <alek.hadzidedic@gmail.com>
2019-10-25 14:47:45 -0400

    did the thing

--- linear_approximation.py
+++ linear_approximation.py
@@ -34,3 +34,36 @@ def construct_sub_linear_approximation(input_sum, output_sum):
             equation_satisfaction_count += 1
 
     return equation_satisfaction_count - 8
+
+
+def exhaust_subkeys(pairs):
+    table = []
+    for key in range(256):
+        bin_str = binary_splice(bin(key), 8)
+        partial_key_1 = bin_str[0:4]
+        partial_key_2 = bin_str[4:]
+
+        counter = 0
+        for pair in pairs:
+            partial_ct_1 = pair[1][0:4]
+            partial_ct_2 = pair[1][12:]
+
+            pt_1 = int('0b' + partial_ct_1, 2) ^ int('0b' + partial_key_1, 2)
+            pt_2 = int('0b' + partial_ct_2, 2) ^ int('0b' + partial_key_2, 2)
+
+            eq_val_1 = s_box_decrypt(pt_1)
+            eq_val_2 = s_box_decrypt(pt_2)
+
+            x1 = binary_splice(bin(eq_val_1), 4)
+            x2 = binary_splice(bin(eq_val_2), 4)
+
+            equation = x1[0] + x1[3] + x2[0] + x2[3] + pair[0][1]
+
+            xor_sum = calculate_xor_sum(equation)
+
+            if xor_sum == 0:
+                counter += 1
+
+        table.append((key, counter))
+
+    return table
--- main.py
+++ main.py
@@ -1,5 +1,11 @@
 from encrypt import generate_plaintext_ciphertext_pairs
+import linear_approximation
 
 round_keys = [3049, 3492, 1038, 357, 6789]
 pairs = generate_plaintext_ciphertext_pairs(10000, round_keys)
-print(pairs)
+#print(pairs)
+
+print(linear_approximation.construct_linear_approximation())
+
+print(linear_approximation.exhaust_subkeys(pairs))
+
