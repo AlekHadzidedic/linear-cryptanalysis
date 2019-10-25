80ac48795ca5cbc768ada4b496acb5103cfcb005
Alek Hadzidedic <alek.hadzidedic@gmail.com>
2019-10-25 00:23:31 -0400

    added keys and sbox functions

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
 import utils
 
+key_1 = bin(0b1100111110010101)
+key_2 = bin(0b0010101000111011)
+key_3 = bin(0b0011001111011101)
+key_4 = bin(0b1110000110100111)
+key_5 = bin(0b1000110010010100)
+
 print(utils.generate_key())
 
--- utils.py
+++ utils.py
@@ -8,3 +8,49 @@ def generate_key():
         key += str(bit)
 
     return key
+
+
+def s_box_encrypt(binary_input):
+    mapping = [
+        bin(12),
+        bin(6),
+        bin(0),
+        bin(4),
+        bin(11),
+        bin(8),
+        bin(2),
+        bin(13),
+        bin(1),
+        bin(15),
+        bin(7),
+        bin(5),
+        bin(14),
+        bin(3),
+        bin(9),
+        bin(10)
+    ]
+
+    return mapping[int(binary_input, 2)]
+
+
+def s_box_decrypt(binary_input):
+    mapping = [
+        bin(2),
+        bin(8),
+        bin(6),
+        bin(13),
+        bin(3),
+        bin(11),
+        bin(1),
+        bin(10),
+        bin(5),
+        bin(14),
+        bin(15),
+        bin(4),
+        bin(0),
+        bin(7),
+        bin(12),
+        bin(9)
+    ]
+
+    return mapping[int(binary_input, 2)]
